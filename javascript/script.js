0be3ad96035ef39a5cb5d07409ca40eca995e7ab
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-23 16:22:54 +0200

    add new_list_email function

--- javascript/script.js
+++ javascript/script.js
@@ -14,23 +14,32 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 const containerlistEl = document.getElementById('containerlist')
 const buttonEl = document.getElementById('bottone')
 
-function new_list_email () {
-    containerlistEl.innerHTML = ''
+function clearList() {
+    containerlistEl.innerHTML = '';
+}
+
+function new_list_email() {
     for (let i = 0; i < 10; i++) {
-    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             const { success, response } = data;
             const list_markup = document.createElement('li');
             list_markup.textContent = response;
             containerlistEl.append(list_markup);
             console.log(list_markup);
         });
-}
+    }
 }
 
-buttonEl.addEventListener('click', new_list_email)
+buttonEl.addEventListener('click', () => {
+    clearList()
+    new_list_email();
+});
+
+
+
+
 
 
 
