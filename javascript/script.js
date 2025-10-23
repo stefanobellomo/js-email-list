fe6663832c2b66916de84a4635d63e2379a7093b
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-23 15:43:08 +0200

    move cycle for in right position

--- javascript/script.js
+++ javascript/script.js
@@ -13,19 +13,19 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 
 const containerlistEl = document.getElementById('containerlist')
 
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-.then(res => res.json())
-.then(data =>{
-    console.log(data);
-    const {success, response} = data
-    for (let i = 0; i < 10; i++) {
-        const thisresponse = response[i];
-        const list_markup = document.createElement('li')
-        list_markup.textContent = response;
-        containerlistEl.append(list_markup)
-        console.log(list_markup);
-    }
-})
+for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const { success, response } = data;
+            const thisresponse = response[i];
+            const list_markup = document.createElement('li');
+            list_markup.textContent = response;
+            containerlistEl.append(list_markup);
+            console.log(list_markup);
+        });
+}
 
 
 
