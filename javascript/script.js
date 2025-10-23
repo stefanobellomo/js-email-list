3a47d63adf26c2965242e2e7ee01fdf2021a7a29
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-23 14:57:26 +0200

    create email array and declare node in js

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+/*
+Descrizione
+
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+*/
+
+
+// genera 10 indirizzi email
+const emailList = [
+    "test1@example.com",
+    "test2@example.com",
+    "test3@example.com",
+    "test4@example.com",
+    "test5@example.com",
+    "test6@example.com",
+    "test7@example.com",
+    "test8@example.com",
+    "test9@example.com",
+    "test10@example.com"
+]
+
+//richiama il nodo nella dom da manipolare
+const containerlistEl = document.getElementById('containerlist')
+
+
+
+
+
